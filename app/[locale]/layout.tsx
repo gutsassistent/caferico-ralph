84f16dcc8b94d93c744e8e0d6651311d6cd1a792
jonathannd02 <jonathan.nunezdhondt@gmail.com>
2026-02-03 09:48:13 +0100

    Fix dependencies adjust UI data

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -53,6 +53,7 @@ export default async function LocaleLayout({ children, params }: LocaleLayoutPro
       <head>
         <link rel="dns-prefetch" href="https://www.caferico.be" />
         <link rel="preconnect" href="https://www.caferico.be" crossOrigin="anonymous" />
+        <link rel="icon" href="/icon.png" />
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: jsonLd(organizationSchema()) }}
